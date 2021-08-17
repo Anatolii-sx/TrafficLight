2ace07005f93c80bc6888a1bb3a8160c8706e77b
Anatolii-sx <s8.8@bk.ru>
2021-08-18 00:57:01 +0300

    Created logic of application

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lights: [UIView]!
+    @IBOutlet var turnLightButton: UIButton!
+    
+    var numberOfLight = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        turnLightButton.layer.cornerRadius = 10
+        
+        lights.forEach {$0.layer.cornerRadius = $0.frame.size.width / 2}
     }
 
+    @IBAction func turnLightButtonPassed(_ sender: UIButton) {
+        turnLightButton.setTitle("NEXT", for: .normal)
+        
+        lights.forEach{$0.alpha = 0.3}
 
+        lights[numberOfLight].alpha = 1
+
+        if numberOfLight < lights.count - 1 {
+            numberOfLight += 1
+        } else {
+            numberOfLight = 0
+        }
+    }
 }
 
