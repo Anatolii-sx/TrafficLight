e3c45b8f706655d0724bbb840d6773fc2055ba9a
Anatolii-sx <s8.8@bk.ru>
2021-08-18 22:46:02 +0300

    Added func viewWillLayoutSubviews and made some little fixes in code (made private property and deleted unused parameter of button function)

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -12,18 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet var lights: [UIView]!
     @IBOutlet var turnLightButton: UIButton!
     
-    var numberOfLight = 0
+    private var numberOfLight = 0
+    
+    override func viewWillLayoutSubviews() {
+        lights.forEach {$0.layer.cornerRadius = $0.frame.size.width / 2}
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         turnLightButton.layer.cornerRadius = 10
-        
-        lights.forEach {$0.layer.cornerRadius = $0.frame.size.width / 2}
     }
 
-    @IBAction func turnLightButtonPassed(_ sender: UIButton) {
-        turnLightButton.setTitle("NEXT", for: .normal)
+    @IBAction func turnLightButtonPassed() {
+        if turnLightButton.currentTitle == "START" {
+            turnLightButton.setTitle("NEXT", for: .normal)
+        }
         
         lights.forEach{$0.alpha = 0.3}
 
